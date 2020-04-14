ba0c19f3dc314d0f4f5b966522f8d1bcfea6a30b
Fabio <fabioagtorres@hotmail.com>
2020-04-14 19:46:53 -0300

    adicionando coisas

--- js/main.js
+++ js/main.js
@@ -7,22 +7,43 @@ function validarIdade(idade){
     }
     return validar;
 }
+
+
+
 function clicou (){
-    document.getElementById("agradecimento").innerHTML = "The Web Whatsapp has been opened at a new Tab";
-    window.open("https://web.whatsapp.com/")
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json/";
+    console.log(url);
+
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response);
+            $("#logradouro").html(response.logradouro);
+            //document.getElementById("logradouro").innerHTML = response.logradouro;
+            document.getElementById("bairro").innerHTML = response.bairro;
+            document.getElementById("localidade").innerHTML = response.localidade;
+            document.getElementById("uf").innerHTML = response.uf;
+        }
+    })
+    document.getElementById("agradecimento").innerHTML = "We're sending a can with 500grams for the location below";
+    //window.open("https://web.whatsapp.com/")
     //window.location.href = "";
     //alert ("Obrigado por clicar");
 }
-function trocar(){
-    document.getElementById("mousemove").innerHTML = "<i>WE APRECIATE YOUR EFFORT ! CHEERS</i>";
+function trocar(elemento){
+    elemento.innerHTML = "<i>WE APRECIATE YOUR EFFORT ! CHEERS</i>";
+    //document.getElementById("mousemove").innerHTML = "<i>WE APRECIATE YOUR EFFORT ! CHEERS</i>";
     //alert("Voce eh um maconheiro safado");
 }
-function voltar(){
-    document.getElementById("mousemove").innerHTML = "<i>WE WOULD LIKE TO GIVE YOU A REWARD FOR YOUR CONTRIBUITION </i>";
+function voltar(elemento){
+    elemento.innerHTML = "<i>WE WOULD LIKE TO GIVE YOU A REWARD FOR YOUR CONTRIBUITION </i>"
+    //document.getElementById("mousemove").innerHTML = "<i>WE WOULD LIKE TO GIVE YOU A REWARD FOR YOUR CONTRIBUITION </i>";
 }
-
-
-
+function load(){
+    alert("Check our new Buds - i mean Partners lol")
+}
 //var idade = prompt("Qual a sua idade?");
 //console.log(validarIdadereward(idade));
 ///
